cde5aa023aa546a5ad1d7002bc438551f6ae7c56
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-02-28 00:38:21 +0100

    Update and rename index.tsx to index.ts

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+/** #SUB_ATOMS */
+
+import Avatar from "./subatoms/Avatar"
+import BrowserEventsToasts from "./subatoms/BrowserEventsToasts"
+import Button from "./subatoms/Button"
+import CheckBox from "./subatoms/CheckBox"
+import FileBox from "./subatoms/FileBox"
+import Form from "./subatoms/Form"
+import InputBox from "./subatoms/InputBox"
+import Link from "./subatoms/Link"
+import List from "./subatoms/List"
+import RadioBox from "./subatoms/RadioBox"
+import TextBox from "./subatoms/TextBox"
+
+/** #ATOMS */
+
+import ClipboardButton from "./atoms/ClipboardButton"
+import ContextSubmitButton from "./atoms/ContextSubmitButton"
+import DownloadFileButton from "./atoms/DownloadFileButton"
+import PrintPageButton from "./atoms/PrintPageButton"
+import RadioBoxList from "./atoms/RadioBoxList"
+import SwitchBox from "./atoms/SwitchBox"
+
+/** #MOLECULES */
+
+import CalendarBox from "./molecules/CalendarBox"
+
+export {
+  Avatar,
+  BrowserEventsToasts,
+  Button,
+  CheckBox,
+  FileBox,
+  Form,
+  InputBox,
+  Link,
+  List,
+  RadioBox,
+  TextBox,
+  
+  ClipboardButton,
+  ContextSubmitButton,
+  DownloadFileButton,
+  PrintPageButton
+  RadioBoxList,
+  SwitchBox,
+
+  CalendarBox
+};
