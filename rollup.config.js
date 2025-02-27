297aee6c9f68154a39bbc95fcdb9ed6f33d7a3ff
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-02-27 23:03:18 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
-import typescript from 'rollup-plugin-typescript2';  
+import typescript from '@rollup/plugin-typescript';  
 
 export default {  
   input: 'src/index.ts',
-  output: {  
+  output: {
     file: 'dist/react-busser-headless-ui.js',
     format: 'umd',
-    name: 'RbhUI'
-  },  
-  plugins: [typescript()]  
+    name: 'RbhUI',
+    sourcemap: true
+  },
+  external: ['react-busser', 'lodash.pick', 'date-fns'],
+  plugins: [typescript()]
 };  
