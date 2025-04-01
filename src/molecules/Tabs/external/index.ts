9e16f99e90ccfb05a7f75407351d8a6c98138183
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-04-01 01:36:19 +0100

    Create index.ts

---
+++ src/molecules/Tabs/external/index.ts
@@ -0,0 +1,23 @@
+import { useSearchParamsState } from "react-busser";
+
+export const useTabs = (
+  activeTabIdQuery = "active_tab__react-busser",
+  activeTabIndex = 0,
+  replaceInHistory = false
+) => {
+  const [, setUrlSearchParamsState] = useSearchParamsState(
+    activeTabIdQuery,
+    replaceInHistory,
+    String(activeTabIndex + 1)
+  );
+  const activate = (currentTabIndex: number) => {
+    setUrlSearchParamsState((previousTabIdQuery) => {
+      if (previousTabIdQuery === String(currentTabIndex + 1)) {
+        return previousTabIdQuery;
+      }
+      return String(currentTabIndex + 1);
+    });
+  };
+
+  return [activate] as const;
+};
