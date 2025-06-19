0979c9ed77d6b01beb9493519451800d010ed2e0
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-06-19 21:07:53 +0100

    chore: code style formatting

--- src/atoms/SearchBarForm/index.tsx
+++ src/atoms/SearchBarForm/index.tsx
@@ -40,16 +40,18 @@ function SearchBarControls({
   searchParamName = "filter",
   wrapperClassName = "",
   textBoxClassName = "",
+  buttonClassName = "",
   textBoxPlaceholder = "Search...",
   textBoxType = "text",
   isBusy: isParentFormSubmitting,
   children,
   ...props
-}: React.ComponentPropsWithRef<"fieldset"> & {
+}: React.ComponentPropsWithRef<"div"> & {
   isBusy: boolean,
   searchParamName?: string,
   wrapperClassName?: string,
   textBoxClassName?: string,
+  buttonClassName?: string;
   textBoxPlaceholder?: string,
   textBoxType?: string,
 }) {
@@ -62,31 +64,56 @@ function SearchBarControls({
 
   return (
     <div {...props} key={filter !== "" ? filter : "default"} role="region">
-      <fieldset role="group">
+      <fieldset role="group" className={wrapperClassName}>
         <input
           name={searchParamName}
           role="searchbox"
+          aria-autocomplete="list"
+          aria-controls={searchParamName}
+          aria-haspopup="menu"
           placeholder={textBoxPlaceholder}
           type={textBoxType}
+          className={textBoxClassName}
           autoComplete="no"
+          spellCheck="false"
           defaultValue={stateValue[searchParamName]}
           ref={inputRef}
         />
         <Button
           disabled={canSetButtonDisabled ? isParentFormSubmitting : false}
           aria-busy={isParentFormSubmitting}
+          className={buttonClassName}
           type={"submit"}
         >
           {children}
         </Button>
       </fieldset>
-      {/*<section id="autocomplete" role="xxx">
-        <ul></ul>
+      {/*<section id={searchParamName} role="menu">
+        <ul role="group">
+          <li role="menuitem"></li>
+          <li role="menuitem"></li>
+        </ul>
       </section>*/}
     </div>
   );
 }
 
 SearchBarForm.Controls = SearchBarControls;
+
+
+/*
+<SearchBarForm searchParamName={"pager"}>
+  <SearchBarForm.Controls
+    className={"..."}
+    buttonClassName={"..."}
+    textBoxClassName={"..."}
+    wrapperClassName={"..."}
+    searchParamName={"pager"}
+    textBoxType={"search"}
+  >
+    <span>Search Me!</span>
+  </SearchBarForm.Controls>
+</SearchBarForm>
+*/
 
 export default SearchBarForm;
