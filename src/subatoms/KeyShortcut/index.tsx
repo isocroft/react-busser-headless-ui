8360ca41fb3ff02dc87093b5447a053d97089ef4
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-06-20 17:43:11 +0100

    wip: creating 'KeyShortcut' component

--- src/subatoms/KeyShortcut/index.tsx
+++ src/subatoms/KeyShortcut/index.tsx
@@ -1,28 +1,62 @@
-import React, { useEffcect } from "react";
+import React, { useEffect } from "react";
 
 function KeyShortcut ({
   children,
-  ...props,
   modifier = "ctrl",
   symbol,
+  ...props,
 }: React.ComponentPropsWithRef<"kbd"> & {
   modifier?: "ctrl" | "alt" | "shift" | "cmd" | "ctrl + shift" | "alt + shift" | "ctrl + alt + shift" | "ctrl + alt",
   symbol: "a" | "s" | "k" | "j" | "z" | "w" | "x" | "f" | "c" | "h" | "d" | "r" | "t" | "u" | "o" | "b" | "q" | "g"
 }) {
   useEffect(() => {
-    const styleText = `
-    kbd[data-hotkeys="shortcut"] {
-      position: relative;
+    const styleSheetsOnly = [].slice.call<StyleSheetList, [], StyleSheet[]>(
+      window.document.styleSheets
+    ).filter(
+      (sheet) => {
+        if (sheet.ownerNode) {
+          return sheet.ownerNode.nodeName === "STYLE";
+        }
+        return false;
+    }).map(
+      (sheet) => {
+        if (sheet.ownerNode
+          && sheet.ownerNode instanceof Element) {
+          return sheet.ownerNode.id;
+        }
+        return "";
+    }).filter(
+      (id) => id !== ""
+    );
+
+    if (styleSheetsOnly.length > 0
+      /* @ts-ignore */
+      && styleSheetsOnly.includes("react-busser-headless-ui_key-shortcut")) {
+      return;
     }
 
-    kbd[data-hotkeys="shortcut"] .modifierKeys {
-      text-transform: capitalize;
-    }
+    const keyShortcutStyle = window.document.createElement('style');
+    keyShortcutStyle.id = "react-busser-headless-ui_key-shortcut";
 
-    kbd[data-hotkeys="shortcut"] small {
-      position: absolute;
-    }
+    keyShortcutStyle.innerHTML = `
+      kbd[data-hotkeys="shortcut"] {
+        position: relative;
+      }
+  
+      kbd[data-hotkeys="shortcut"] .modifierKeys {
+        text-transform: capitalize;
+      }
+  
+      kbd[data-hotkeys="shortcut"] small {
+        position: absolute;
+      }
     `;
+
+    window.document.head.appendChild(keyShortcutStyle);
+
+    return () => {
+      window.document.head.removeChild(keyShortcutStyle);
+    };
   }, []);
   return (
     <kbd {...props} data-hotkeys="shortcut">
@@ -34,17 +68,21 @@ function KeyShortcut ({
   )
 }
 
-KeyShortcut.DescriptionTip = ({ text }: { text: string ) => {
-  return (<small>{text}</small>);
+const DescText = ({ text }: { text: string }) => {
+  return <small>{text}</small>;
 };
 
+KeyShortcut.Description = DescText;
+
 /*
+
 <KeyShortcut
   modifier={"ctrl + alt"}
   symbol="a"
 >
-  <KeyShortcut.DescriptionTip text="go fullscreen" />
+  <KeyShortcut.Description text="go fullscreen" />
 </KeyShortcut>
+
 */
 
 export default KeyShortcut;
