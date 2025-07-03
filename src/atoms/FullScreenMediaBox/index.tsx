940229caa9c994ca135b7bd6c9edacd475855889
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2025-07-03 18:40:50 +0100

    Update index.tsx

--- src/atoms/FullScreenMediaBox/index.tsx
+++ src/atoms/FullScreenMediaBox/index.tsx
@@ -4,11 +4,85 @@ import { useFullscreen, useHotKeys } from "@mantine/hooks";
 
 import type { ButtonProps } from "../../subatoms/Button";
 
+/**
+ * isYouTubeUrl:
+ *
+ * @param {String} url
+ *
+ * @returns {Boolean}
+ */
+const isYouTubeUrl = (url?: string | null) => {
+  if (!url || typeof url !== "string") {
+    throw new Error("isYouTubeUrl(...): argument `url` is not a string");
+  }
+  return url.includes('youtube.com') || url.includes('youtu.be');
+};
+
+/**
+ * isVimeoUrl:
+ *
+ * @param {String} url
+ *
+ * @returns {Boolean}
+ */
+const isVimeoUrl = (url?: string | null) => {
+  if (!url || typeof url !== "string") {
+    throw new Error("iisVimeoUrl(...): argument `url` is not a string");
+  }
+  return url.includes('vimeo.com') || url.includes('player.vimeo.com');
+};
+
+/**
+ * getEmbedUrl:
+ *
+ * @param {String | Null} url
+ * @param {Boolean} autoPlay
+ *
+ *
+ * @returns {String}
+ *
+ */
+const getEmbedUrl = (url: string | null, autoPlay = false): string => {
+  if (!url || typeof url !== "string" || typeof autoPlay !== "boolean") {
+    throw new Error("getEmbedUrl(...): argument `url` or `autoPlay` is not valid");
+  }
+
+  try {
+    const parsedUrl = new URL(url);
+    const domain = parsedUrl.hostname.replace('www.', '').replace('m.', '');
+    let videoId = '';
+    if (domain === 'youtube.com' || domain === 'youtu.be') {
+      if (parsedUrl.pathname.includes('/embed/')) {
+        return `${url}${autoPlay ? '?&autoplay=1&mute=1' : ''}`;
+      }
+      if (parsedUrl.pathname.includes('/watch')) {
+        videoId = parsedUrl.searchParams.get('v') || '';
+        return `https://www.youtube.com/embed/${videoId}${autoPlay ? '?&autoplay=1&mute=1' : ''}`;
+      }
+      if (domain === 'youtu.be') {
+        videoId = parsedUrl.pathname.replace('/', '');
+        return `https://www.youtube.com/embed/${videoId}${autoPlay ? '?&autoplay=1&mute=1' : ''}`;
+      }
+    } else if (domain === 'vimeo.com' || domain === 'player.vimeo.com') {
+      if (parsedUrl.pathname.includes('/video/')) {
+        return `${url}${autoPlay ? '?&autoplay=1&muted=1' : ''}`;
+      }
+      videoId = parsedUrl.pathname.replace('/', '');
+      return `https://player.vimeo.com/video/${videoId}${autoPlay ? '?&autoplay=1&muted=1' : ''}`;
+    }
+  } catch (error) {
+    console.error('Invalid URL:', error);
+    throw error;
+  }
+  return '';
+};
+
 const FullScreenMediaBox = ({
   children,
   type = "image",
   width = 300,
   height = 100,
+  autoPlay = false,
   alt = "fullscreen media element",
   src,
   className,
@@ -20,6 +94,7 @@ const FullScreenMediaBox = ({
   src: string,
   width?: number,
   height?: number,
+  autoPlay?: boolean,
   alt?: string,
   wrapperClassName?: string,
   onClick?: ButtonProps["onClick"]
@@ -90,6 +165,36 @@ const FullScreenMediaBox = ({
     }
   }, []);
 
+  const getMediaContent = () => {
+    if (isYouTubeUrl(src) || isVimeoUrl(src)) {
+      return (
+        <iframe
+          width={width}
+          height={height}
+          ref={mediaElemRef}
+          title="embed video"
+          src={getEmbedUrl(src, autoPlay)}
+          allow={`accelerometer${autoPlay ? " autoplay" : ""}; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share`}
+          referrerPolicy={"strict-origin-when-cross-origin"}
+          allowFullScreen
+        />
+      );
+    }
+
+    return (
+      <video
+        src={src}
+        ref={mediaElemRef}
+        width={width}
+        height={height}
+        data-fullscreen-mode={fullscreen ? "true" : "false"}
+        className={className}
+        autoPlay={autoPlay ? "true" : undefined}
+        alt={alt}
+      ></video>
+    );
+  };
+
   return (
     <section {...props} className={wrapperClassName}>
       <button
@@ -112,15 +217,7 @@ const FullScreenMediaBox = ({
             alt={alt}
           />
         :
-          <video
-            src={src}
-            ref={mediaElemRef}
-            width={width}
-            height={height}
-            data-fullscreen-mode={fullscreen ? "true" : "false"}
-            className={className}
-            alt={alt}
-          ></video>
+          getMediaContent()
       }
       {children}
     </section>
