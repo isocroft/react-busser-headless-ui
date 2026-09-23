781feb1a2caf7294fcacbf2982c6988a969be5dc
Ifeora Okechukwu Patrick <isocroft@users.noreply.github.com>
2026-09-23 13:25:55 +0100

    Create index.tsx

---
+++ src/subatoms/Table/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import type { FC } from "react";
+
+
+function Table({
+  children,
+  ...props
+}: React.ComponentPropsWithRef<"table">) => {
+  return null;
+};
+
+export default Table;
